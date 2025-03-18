fab25071fc08a44469b9d4125c3b10c97dad11bd
Samudra Putra <samudrapg28@gmail.com>
2025-03-18 09:40:35 +0700

    first commit

---
+++ BodyNRSL.c
@@ -0,0 +1,166 @@
+/* File        : bodyNRSL.c */
+/* Deskripsi   : Body prototype ADT Non Restricted Single Linked/Linear list yang hanya dikenali First(L) */
+/* Dibuat oleh : Ade Chandra Nugraha */
+/* Tanggal     : 24-10-2001 */
+
+
+#include <stdio.h>
+#include <boolean.h>
+#include <limits.h>
+#include <malloc.h>
+#include "spnrsll.h"
+
+/* Mengecek apakah List kosong */
+boolean ListEmpty(List L) {
+    return (First(L) == Nil);
+}
+
+/* Membuat List kosong */
+void CreateList(List *L) {
+    First(*L) = Nil;
+}
+
+/* Mengalokasikan memori untuk elemen baru */
+address Alokasi(infotype X) {
+    address P = (address)malloc(sizeof(ElmtList));
+    if (P != Nil) {
+        Info(P) = X;
+        Next(P) = Nil;
+    }
+    return P;
+}
+
+/* Dealokasi elemen */
+void DeAlokasi(address P) {
+    free(P);
+}
+
+/* Mencari elemen dengan nilai X */
+address Search(List L, infotype X) {
+    address P = First(L);
+    while (P != Nil) {
+        if (Info(P) == X) return P;
+        P = Next(P);
+    }
+    return Nil;
+}
+
+/* Mencari apakah elemen beralamat P ada di list */
+boolean FSearch(List L, address P) {
+    address Temp = First(L);
+    while (Temp != Nil) {
+        if (Temp == P) return true;
+        Temp = Next(Temp);
+    }
+    return false;
+}
+
+/* Mencari elemen sebelum elemen dengan nilai X */
+address SearchPrec(List L, infotype X) {
+    address Prec = Nil, P = First(L);
+    while (P != Nil && Info(P) != X) {
+        Prec = P;
+        P = Next(P);
+    }
+    return (P == Nil) ? Nil : Prec;
+}
+
+/* Menambahkan elemen di awal */
+void InsVFirst(List *L, infotype X) {
+    address P = Alokasi(X);
+    if (P != Nil) {
+        Next(P) = First(*L);
+        First(*L) = P;
+    }
+}
+
+/* Menambahkan elemen di akhir */
+void InsVLast(List *L, infotype X) {
+    address P = Alokasi(X);
+    if (P != Nil) {
+        if (ListEmpty(*L)) {
+            First(*L) = P;
+        } else {
+            address Last = First(*L);
+            while (Next(Last) != Nil) {
+                Last = Next(Last);
+            }
+            Next(Last) = P;
+        }
+    }
+}
+
+/* Menghapus elemen pertama */
+void DelVFirst(List *L, infotype *X) {
+    if (!ListEmpty(*L)) {
+        address P = First(*L);
+        *X = Info(P);
+        First(*L) = Next(P);
+        DeAlokasi(P);
+    }
+}
+
+/* Menghapus elemen terakhir */
+void DelVLast(List *L, infotype *X) {
+    if (!ListEmpty(*L)) {
+        address P = First(*L), Prec = Nil;
+        while (Next(P) != Nil) {
+            Prec = P;
+            P = Next(P);
+        }
+        *X = Info(P);
+        if (Prec == Nil) {
+            First(*L) = Nil;
+        } else {
+            Next(Prec) = Nil;
+        }
+        DeAlokasi(P);
+    }
+}
+
+/* Menambahkan elemen setelah elemen tertentu */
+void InsertAfter(List *L, address P, address Prec) {
+    if (P != Nil && Prec != Nil) {
+        Next(P) = Next(Prec);
+        Next(Prec) = P;
+    }
+}
+
+/* Menghapus elemen tertentu dalam list */
+void DelP(List *L, infotype X) {
+    address P = First(*L), Prec = Nil;
+    while (P != Nil && Info(P) != X) {
+        Prec = P;
+        P = Next(P);
+    }
+    if (P != Nil) {
+        if (Prec == Nil) {
+            First(*L) = Next(P);
+        } else {
+            Next(Prec) = Next(P);
+        }
+        DeAlokasi(P);
+    }
+}
+
+/* Mencetak isi List */
+void PrintInfo(List L) {
+    address P = First(L);
+    if (P == Nil) {
+        printf("List Kosong\n");
+    } else {
+        while (P != Nil) {
+            printf("%d -> ", Info(P));
+            P = Next(P);
+        }
+        printf("Nil\n");
+    }
+}
+
+/* Menghapus semua elemen dalam List */
+void DelAll(List *L) {
+    infotype X;
+    while (!ListEmpty(*L)) {
+        DelVFirst(L, &X);
+    }
+}
---
+++ MainNRSL.c
@@ -0,0 +1,85 @@
+/* File        : mainNRSL.cpp */
+/* Deskripsi   : Driver / main program untuk ADT Non Restricted Single Linked/Linear List */
+/* Dibuat oleh : Ade Chandra Nugraha*/
+/* Tanggal     : 25-10-2001 */
+/* Diupdate oleh : Santi Sundari, 6-4-2011, hanya tdd operasi2 dasar */
+/* Diupdate oleh : WW & AM, 8-3-2025, penyesuaian area body & main untuk latihan Diploma 3 */
+
+#include "SpNRSLL.h"
+#include <stdio.h>
+
+int main() {
+    /* Kamus Lokal */
+    List MyList;
+    infotype isi;
+    address P, Prec;
+
+    /* Program */
+    CreateList(&MyList); // Membuat list kosong
+    printf("List telah dibuat kosong.\n");
+
+    // 1. Insert First(7)
+    printf("Menambahkan 7 di awal list.\n");
+    InsVFirst(&MyList, 7);
+    PrintInfo(MyList);
+
+    // 2. Insert Last(11)
+    printf("Menambahkan 11 di akhir list.\n");
+    InsVLast(&MyList, 11);
+    PrintInfo(MyList);
+
+    // 3. Insert After(9 setelah 7)
+    printf("Menambahkan 9 setelah 7.\n");
+    P = Alokasi(9);
+    Prec = Search(MyList, 7);
+    if (P != Nil && Prec != Nil) {
+        InsertAfter(&MyList, P, Prec);
+    } else {
+        printf("Gagal menambahkan 9 setelah 7.\n");
+    }
+    PrintInfo(MyList);
+
+    // 4. Insert First(5)
+    printf("Menambahkan 5 di awal list.\n");
+    InsVFirst(&MyList, 5);
+    PrintInfo(MyList);
+
+    // 5. Insert Last(13)
+    printf("Menambahkan 13 di akhir list.\n");
+    InsVLast(&MyList, 13);
+    PrintInfo(MyList);
+
+    // 6. Delete Last (hapus 13)
+    if (!ListEmpty(MyList)) {
+        DelVLast(&MyList, &isi);
+        printf("Elemen terakhir dengan nilai %d telah dihapus.\n", isi);
+    } else {
+        printf("Gagal menghapus elemen terakhir, list kosong.\n");
+    }
+    PrintInfo(MyList);
+
+    // 7. Delete Specific (hapus 7)
+    printf("Menghapus elemen dengan nilai 7.\n");
+    if (Search(MyList, 7) != Nil) {
+        DelP(&MyList, 7);
+    } else {
+        printf("Gagal menghapus 7, elemen tidak ditemukan.\n");
+    }
+    PrintInfo(MyList);
+
+    // 8. Delete First (hapus 5)
+    if (!ListEmpty(MyList)) {
+        DelVFirst(&MyList, &isi);
+        printf("Elemen pertama dengan nilai %d telah dihapus.\n", isi);
+    } else {
+        printf("Gagal menghapus elemen pertama, list kosong.\n");
+    }
+    PrintInfo(MyList);
+
+    // 9. Delete All (hapus semua elemen)
+    printf("Menghapus semua elemen dalam list.\n");
+    DelAll(&MyList);
+    PrintInfo(MyList);
+
+    return 0;
+}
